4e101b1c8da107a70d83fe3e2a5d9d211e19099e
wildanJerry <wildan@rnd.ts.co.id>
2025-05-27 20:04:10 +0700

    Flask + OpenCV

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-# main.py
 from website import create_app
 
-# create_app sekarang mengembalikan tuple (app, socketio)
-app, socketio = create_app() # Tangkap kedua nilai yang dikembalikan
+app = create_app()
 
 if __name__ == '__main__':
-    # Anda harus menggunakan socketio.run() untuk menjalankan aplikasi Flask-SocketIO
-    # Jangan menggunakan app.run() lagi
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
-    # debug=True: Untuk mode pengembangan, otomatis reload saat ada perubahan kode
-    # host='0.0.0.0': Agar bisa diakses dari jaringan lokal (bukan hanya localhost)
-    # port=5000: Port default Flask
+    app.run(debug=True)
